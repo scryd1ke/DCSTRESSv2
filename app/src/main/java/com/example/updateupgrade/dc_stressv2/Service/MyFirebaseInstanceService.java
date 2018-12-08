d71f018b435ec1d9f2a1bcea0d898c50addd8339
scryd1ke <scryd1ke@gnail.com>
2018-12-08 19:27:42 -0300

    FD-DCSTRESSV2 SIMPLE

--- app/src/main/java/com/example/updateupgrade/dc_stressv2/Service/MyFirebaseInstanceService.java
+++ app/src/main/java/com/example/updateupgrade/dc_stressv2/Service/MyFirebaseInstanceService.java
@@ -14,6 +14,7 @@ import com.example.updateupgrade.dc_stressv2.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
 import java.util.Random;
 
 public class MyFirebaseInstanceService extends FirebaseMessagingService {
@@ -22,9 +23,51 @@ public class MyFirebaseInstanceService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+        if (remoteMessage.getData().isEmpty())
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        else
+            showNotification(remoteMessage.getData());
         
+
+
     }
+
+    private void showNotification(Map<String,String> data) {
+
+        String title = data.get("title").toString();
+        String body = data.get("body").toString();
+
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.example.updateupgrade.dc_stressv2"; //NO SE DE DONDE SALE TEST
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification", NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("DCSTRESS Channel"); // CANAL DE MENSAJES CORROBORAR CON FIREBASE
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.WHITE);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+
+        NotificationCompat.Builder notificationBuilder  = new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("info");
+
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+
+    }
+
 
     private void showNotification(String title, String body) {
 

26f476ca7dea6f52154410d30e64471ffb8e1ef2
scryd1ke <scryd1ke@gnail.com>
2018-12-08 15:46:21 -0300

    FD- llenado el repositorio con el proyecto v2

---
+++ app/src/main/java/com/example/updateupgrade/dc_stressv2/Service/MyFirebaseInstanceService.java
@@ -0,0 +1,66 @@
+package com.example.updateupgrade.dc_stressv2.Service;
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.example.updateupgrade.dc_stressv2.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+public class MyFirebaseInstanceService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+        
+    }
+
+    private void showNotification(String title, String body) {
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.example.updateupgrade.dc_stressv2.test"; //NO SE DE DONDE SALE TEST
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification", NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("DCSTRESS Channel"); // CANAL DE MENSAJES CORROBORAR CON FIREBASE
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.WHITE);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+
+        NotificationCompat.Builder notificationBuilder  = new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("info");
+
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.d("TOKENFIREBASE",s);
+    }
+}
